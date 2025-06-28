cda95fdd3f0122a7e675cee21e6ff7d44be30a37
tusar-verma <tusarverma12345@gmail.com>
2025-06-28 20:11:51 -0300

    Agregado al data downloader test sets

--- dataDownloader.py
+++ dataDownloader.py
@@ -6,6 +6,14 @@ import io
 
 ###### todavia no recorte el dataset de gatos y perros para que sean de 300-ish imagenes #####
 
+dict_test = {
+    "cars": "https://www.dropbox.com/scl/fi/kf6lxlu6ayu30v5wz77cc/cars_test.zip?rlkey=fxpr0vawbzhgd0r5u5r4fwfv8&st=sqqgpu8j&dl=0",
+    "cats": "https://www.dropbox.com/scl/fi/i09lf4c4849yxfsett3su/cats_test.zip?rlkey=hpwbi4qcbc0s5uu5jyyasma0b&st=so8h59x7&dl=0",
+    "dogs": "https://www.dropbox.com/scl/fi/5axm0tqvxcuaxyg7zxhcf/dogs_test.zip?rlkey=51fwuosdslkxibtdozjscmc1s&st=kkhz53n2&dl=0",
+    "landscapes": "https://www.dropbox.com/scl/fi/e8hswg4uz2ibo5lkr2wic/landscape_test.zip?rlkey=rjles5o0d4ar33i7nx2vjvezy&st=23mryl5r&dl=0",
+    "people" : "https://www.dropbox.com/scl/fi/5xhhvsp2iwp4vikqg7scm/people_validate.zip?rlkey=t2bt5aqupz6u6z43t23arjjkj&st=lxv79tgc&dl=0"
+}
+
 dict_source = {
     "bsd300": "https://www.dropbox.com/scl/fi/kxvxne6w3maal50mh6na3/BSD300.zip?rlkey=bnul455xvn1ylnpr2aes10xok&st=vojdg78q&dl=0",
     "cars": "https://www.dropbox.com/scl/fi/iah6sccsrpetgq5tbp0b0/cars.zip?rlkey=gchvhhp02pvnj8o0256mppckg&st=im29ir3k&dl=0",
@@ -15,7 +23,7 @@ dict_source = {
     "people" : "https://www.dropbox.com/scl/fi/tvljbws41feghltb064wf/people.zip?rlkey=25u0j55sgsyhxwkcwoug01nf5&st=js8dae4d&dl=0"
 }
 
-def downloadData(dropboxLink, outDirName):
+def downloadData(dropboxLink, output_dir):
     """
     Downloads a ZIP file from a Dropbox shared link and extracts it into ./data/outDirName
     Only downloads and extracts if the directory does not already exist or is empty.
@@ -32,7 +40,6 @@ def downloadData(dropboxLink, outDirName):
     else:
         raise ValueError("Invalid Dropbox link. It should end with 'dl=0' or 'dl=1'.")
 
-    output_dir = os.path.join('dataSets', outDirName)
     # Check if directory exists and is not empty
     if os.path.isdir(output_dir) and os.listdir(output_dir):
         print(f"Data already exists in ./{output_dir}, skipping download.")
@@ -50,6 +57,20 @@ def downloadData(dropboxLink, outDirName):
 
     print("Download and extraction complete.")
 
+def downloadTestData(testName):
+    """
+    Downloads a specific test dataset based on its name.
+    Currently supports bsd300, cars, cats, dogs, landscapes, people dataset.
+    
+    Args:
+        testName (str): The name of the test dataset to download.
+    """
+    
+    if testName in dict_test:
+        downloadData(dict_test[testName], os.path.join('testSets', testName))
+    else:
+        raise ValueError(f"Test dataset '{testName}' is not recognized. Available datasets: {list(dict_test.keys())}")
+
 
 def downloadDataSet(datasetName):
     """
@@ -61,7 +82,7 @@ def downloadDataSet(datasetName):
     """
    
     if datasetName in dict_source:
-        downloadData(dict_source[datasetName], datasetName)
+        downloadData(dict_source[datasetName], os.path.join('dataSets', datasetName))
     else:
         raise ValueError(f"Dataset '{datasetName}' is not recognized. Available datasets: {list(dict_source.keys())}")
 
